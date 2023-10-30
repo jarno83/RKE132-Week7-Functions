aba153df17104203cd8ad5db349b55a4135b13a8
Jarno Orasmäe <jarnoorasmae@tktk.ee>
2023-10-31 00:00:06 +0200

    add week 7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+
+
+static void PrintHello() //Function /method
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
